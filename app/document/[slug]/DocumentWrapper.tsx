37dd9adaf97b221037a735615236c6f9a75b3ee1
Victory Nwani <vickywane@gmail.com>
2024-06-14 18:12:29 +0100

    update: implement data fetching in dynamic document response

---
+++ app/document/[slug]/DocumentWrapper.tsx
@@ -0,0 +1,143 @@
+"use client";
+import Button from "@/components/buttons";
+import { AxiosClient } from "@/utils/AxiosClient";
+import { firebaseApp } from "@/utils/firebase";
+import { getAuth } from "firebase/auth";
+import { useEffect, useState } from "react";
+import { IoIosSync } from "react-icons/io";
+
+const formatTitle = (title: string) => title.split("-").join(" ");
+
+const DocumentWrapper = ({ docName }: { docName: string }) => {
+  const [authToken, setAuthToken] = useState<null | "">(null);
+  const [document, setDocument] = useState<null | Array<any>>(null);
+  const [isLoading, setLoading] = useState(false);
+  const [syncResult, setSyncResult] = useState({ status: "", message: "" });
+
+  const authInstance = getAuth(firebaseApp);
+
+  useEffect(() => {
+    if (authToken) {
+      (async () => await getDocumentData(docName))();
+    }
+  }, [authToken]);
+
+  authInstance?.onAuthStateChanged((user) => setAuthToken(user?.accessToken));
+
+  const getDocumentData = async (slug: string) => {
+    const request = await AxiosClient({
+      endpoint: `document?slug=${slug}`,
+      authToken,
+    });
+
+    setDocument(request?.data);
+  };
+
+  const handleDataSync = async (documentId: string) => {
+    setLoading(true);
+
+    try {
+      const syncRequest = await AxiosClient({
+        endpoint: "sync",
+        method: "POST",
+        authToken,
+        body: {
+          documentId,
+        },
+      });
+
+      setSyncResult({
+        status: syncRequest?.data?.status,
+        message: syncRequest?.data?.message,
+      });
+    } catch (error) {
+      setSyncResult({
+        status: "SYNC_ERROR",
+        message:
+          "Couldn't connect to ETA's services. Are you connected to the internet?",
+      });
+    } finally {
+      setLoading(false);
+
+      setTimeout(() => {
+        setSyncResult({ status: "", message: "" });
+      }, 10000);
+    }
+  };
+
+  return (
+    <div className="max-w-[1100px] m-auto">
+      <div className="flex flex-row justify-between my-4 ">
+        <div className="flex items-center">
+          <h1 className="text-2xl capitalize">{formatTitle(docName)}</h1>
+        </div>
+
+        <div>
+          <Button
+            icon={<IoIosSync size={24} />}
+            text={isLoading ? "Syncing" : "Sync"}
+            clickAction={() => handleDataSync(docName)}
+          />
+        </div>
+      </div>
+
+      <hr />
+
+      <br />
+
+      {!document ? (
+        <div>
+          <p>Loading data in document</p>
+        </div>
+      ) : (
+        <div>
+          <div className="relative overflow-x-auto">
+            <table className="w-full text-sm text-left rtl:text-right text-gray-600">
+              <thead className="text-xs text-gray-700 uppercase bg-gray-300">
+                <tr>
+                  {document?.tracking?.map((item, index) => (
+                    <th key={index} scope="col" className="px-6 py-3">
+                      {item}
+                    </th>
+                  ))}
+                </tr>
+              </thead>
+
+              <tbody>
+                {document?.data?.map((item, index) => (
+                  <tr className="bg-white border-b">
+                    <th
+                      scope="row"
+                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                    >
+                      <div>
+                        <p>{item?.companyName}</p>
+                      </div>
+                    </th>
+                    <td className="px-6 py-4">
+                      <div>
+                        <p>{item?.dateApplied}</p>
+                      </div>
+                    </td>
+                    <td className="px-6 py-4">
+                      <div>
+                        <p>{item?.status}</p>
+                      </div>
+                    </td>
+                    <td className="px-6 py-4">
+                      <div>
+                        <p>{item?.companyName}</p>
+                      </div>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default DocumentWrapper;
--- app/document/[slug]/page.tsx
+++ app/document/[slug]/page.tsx
@@ -3,33 +3,14 @@ import { AxiosClient } from "@/utils/AxiosClient";
 import { firebaseApp } from "@/utils/firebase";
 import { getAuth } from "firebase/auth";
 import { useState } from "react";
-
-const getData = async (docName: string) => {
-  // const request = await AxiosClient({
-  //   endpoint: "/document",
-  //   authToken: "",
-  // });
-
-  // return request?.data;
-};
+import DocumentWrapper from "./DocumentWrapper";
 
 const Page = async ({ params }) => {
-  const authInstance = getAuth(firebaseApp);
-
-  const [authToken, setAuthToken] = useState<null | "">(null);
-
-
-  authInstance?.onAuthStateChanged((user) => setAuthToken(user?.accessToken));
-
-  console.log("authInstance", authToken);
-
-  // const { data } = await getData(params?.slug);
-
   return (
     <div>
       <Header />
 
-      <h1> DOCUMENT PAGE HERE </h1>
+      <DocumentWrapper docName={params?.slug} />
     </div>
   );
 };
